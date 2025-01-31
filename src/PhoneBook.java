3e2d0a2d79d25fe857551f41647bec6c9d18b3f5
Yasmin Woodlock <yasminwoodlock@gmail.com>
2025-01-31 10:59:21 +0000

    Beginning phone book

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    TreeMap<Integer, Address> phoneAddress;
+
+    public PhoneBook(){
+        phoneAddress = new TreeMap<>();
+    }
+
+    public void add(String nickname, String firstName, String surname, String mobileNumber, String homeNumber, String businessNumber, String email, Date birthday){
+    }
+
+    public void update(){
+    }
+
+    public void delete(){
+
+    }
+
+    public Address getAddress(int id){
+        return phoneAddress.get(id);
+    }
+
+    public void setEmergencyContact(boolean emergencyContact){
+
+    }
 }
