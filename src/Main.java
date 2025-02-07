27988cda6d3f1f5f0d4bd4ab2397e813f1cb0c8d
Yasmin Woodlock <yasminwoodlock@gmail.com>
2025-02-07 12:02:26 +0000

    removed test() from main

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void test() {
-        PhoneBook myPhoneBook = new PhoneBook("test.csv");
-        myPhoneBook.add(new Contact("Yasmin", "Yasmin", "Woodlock", "0899583792", "0526135952", "1234567890", new Date(12, 11, 2003)));
-        myPhoneBook.add(new Contact("Yas", "Yasmin", "Lock", "0899583792", "0526135952", "1234567890", new Date(12, 11, 2003)));
-        myPhoneBook.add(new Contact("Y", "April", "Gilhool", "1234567890", "1234567890", "1234567890", new Date(12, 11, 2004)));
-        //myPhoneBook.add(new Contact("Y", "April", "Gilhool", "1234590", "1234567890", "1234567890", new Date(12, 11, 2004)));
-
-
-        System.out.println(myPhoneBook);
-
-        System.out.println("SEARCH BY NAME");
-        System.out.println(myPhoneBook.searchByName("Yasmin"));
-
-        myPhoneBook.delete(2);
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         PhoneBook myPhoneBook = new PhoneBook("test.csv");
